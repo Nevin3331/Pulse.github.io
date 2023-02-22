b86a7686c4e2862f8dd64fba2108faa4e68b818a
nevin <112953697+Nevin3331@users.noreply.github.com>
2023-02-22 16:10:02 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,121 @@
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+        speed: 1200,/* 
+        adaptiveHeight: true, */
+        prevArrow: '<button type="button" class="slick-prev"><img src="logo/lr/left.svg"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="logo/lr/right.svg"></button>',
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                    dots: true,
+                    arrows: false
+                }
+            }
+        ]
+    });
+
+    $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+        $(this)
+            .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+            .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+    });
+
+    $('.catalog-item__link').each(function(i) {
+        $(this).on('click', function(e) {
+            e.preventDefault();
+            $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+            $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+        })
+    });
+
+    $('.catalog-item__back').each(function(i) {
+        $(this).on('click', function(e) {
+            e.preventDefault();
+            $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+            $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+        })
+    });
+
+    function toggleSlide(item) {
+        $('.catalog-item__item').each(function(i) {
+            $(this).on('click', function(e) {
+                e.preventDefault();
+                $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+                $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+            })
+        });
+    };
+
+    toggleSlide('.catalog-item__link')
+    toggleSlide('.catalog-item__back')
+
+    //Modal
+
+    //consultation
+    $('[data-modal=consultation]').on('click', function() {
+        $('.overlay, #consultation').fadeIn('slow');
+    });
+    $('.modal__close').on('click', function() {
+        $('.overlay, #consultation, #order, #thanks').fadeOut('slow');
+    });
+
+    //order
+    $('[data-modal=order]').on('click', function() {
+        $('.overlay, #order').fadeIn('slow');
+    });
+    $('.modal__close').on('click', function() {
+        $('.overlay, #consultation, #order, #thanks').fadeOut('slow');
+    });
+    //each tab card
+    $('.button_mini').each(function(i) {
+        $(this).on('click', function() {
+            $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+            $('.overlay, #order').fadeIn('slow');
+        })
+    });
+
+    //thanks
+    $('[data-modal=thanks]').on('click', function() {
+        $('.overlay, #thanks').fadeIn('slow');
+    });
+    $('.modal__close').on('click', function() {
+        $('.overlay, #consultation, #order, #thanks').fadeOut('slow');
+    });
+
+
+    //VALIDATION FORM
+
+    function validaForms(form){
+        $(form).validate({
+            rules: {
+                name: {
+                    required: true,
+                    minlenght: 2
+                },
+                phone: "required",
+                email: {
+                    required: true,
+                    email: true
+                }
+            },
+            messages: {
+                name: {
+                    required: "Пожалуйста, введите своё имя",
+                    minlenght: jQuery.validator.format("Введите {0} символа!")
+                },
+                phone: "Пожалуйста, введите свой номер",
+                email: {
+                    required: "Пожалуйста, введите свою почту",
+                    email: "Неправильно введён адрес почты"
+                }
+            }
+        });
+    };
+
+    validaForms('#consultation-form');
+    validaForms('#consultation form');
+    validaForms('#order form');
+
+    $('input[name=phone]').mask("+380 (99) 999-99-99");
+});
